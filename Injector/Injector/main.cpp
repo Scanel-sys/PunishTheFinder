8468f5cf4b7bb82c780bf3b69fa130c74954e043
Scanel-sys <nikitabilan2002@gmail.com>
2024-04-18 18:46:20 +0300

    s2ws moved to static class

--- Injector/Injector/main.cpp
+++ Injector/Injector/main.cpp
@@ -1,17 +1,9 @@
 #include "Injector.h"
 
-std::wstring s2ws(const std::string& str)
-{
-	int size_needed = MultiByteToWideChar(CP_UTF8, 0, &str[0], (int)str.size(), NULL, 0);
-	std::wstring wstrTo(size_needed, 0);
-	MultiByteToWideChar(CP_UTF8, 0, &str[0], (int)str.size(), &wstrTo[0], size_needed);
-	return wstrTo;
-}
-
 
 int main(int argc, char * argv[])
 {
-	std::wstring proc_name = s2ws(argv[1]);
+	std::wstring proc_name = Injector::s2ws(argv[1]);
 	fs::path dll_path = argv[2];
 
 	Injector injector;
